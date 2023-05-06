302b92a647d2a730dc166894099bc35ac7cbe279
Ankit Kumar <ankitkr0696@gmail.com>
2023-05-06 13:21:33 +0530

    first commit

---
+++ src/api/hostel/controllers/hostel.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * hostel controller
+ */
+
+const { createCoreController } = require('@strapi/strapi').factories;
+
+module.exports = createCoreController('api::hostel.hostel');
---
+++ src/api/hostel/routes/hostel.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * hostel router
+ */
+
+const { createCoreRouter } = require('@strapi/strapi').factories;
+
+module.exports = createCoreRouter('api::hostel.hostel');
